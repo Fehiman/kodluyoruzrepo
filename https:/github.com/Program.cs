b18402fff9c20edcd5371036686d7d2067bc6a63
Fehiman <95622259+Fehiman@users.noreply.github.com>
2022-04-01 00:51:21 +0300

    odev2
    https://app.patika.dev/courses/csharp-101/5-operatorler

---
+++ https:/github.com/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace odev2
+{
+    class Program
+    {
+        static async void Main(string[] args)
+        {
+            Console.WriteLine("Atama ve işlemli atama operatrleri");
+
+            int x=3;
+            int y=3;
+            y=y+2;
+
+            Console.WriteLine(y);
+            y+=2;
+            Console.WriteLine(y);
+            y/=1;
+            Console.WriteLine(y);
+            x*=2;
+            Console.WriteLine(x);
+
+            Console.WriteLine(y);
+
+            Console.WriteLine("Mantıksal operatörler");
+
+            bool isSuccess=true;
+            bool isCompleted=false;
+            
+            if(isSuccess && isCompleted)
+                Console.WriteLine("Perfect");
+            
+            if(isSuccess || isCompleted)
+                Console.WriteLine("Great");
+
+            if(isSuccess && isCompleted)
+                Console.WriteLine("Fine");
+            
+            Console.WriteLine("İLişkisel operatörler");
+
+            int a=3; 
+            int b=4;
+            bool sonuc=a<b;
+
+            Console.WriteLine(sonuc);
+            sonuc=a>b;
+            Console.WriteLine(sonuc);
+            sonuc=a>=b;
+            Console.WriteLine(sonuc);
+            sonuc=a<=b;
+            Console.WriteLine(sonuc);
+            sonuc=a==b;
+            Console.WriteLine(sonuc);
+            sonuc=a!=b;
+            Console.WriteLine(sonuc);
+
+            Console.WriteLine("Aritmetik operatrler");
+
+            int sayi1=10;
+            int sayi2=5;
+            int sonuc1=sayi1/sayi2;
+            Console.WriteLine(sonuc1);
+            sonuc1=sayi1*sayi2;
+            Console.WriteLine(sonuc1);
+            sonuc1=sayi1+sayi2;
+            Console.WriteLine(sonuc1);
+            sonuc1=sayi1++;
+            Console.WriteLine(sonuc1);
+
+            int sonuc2=20%3;
+            Console.WriteLine(sonuc2);
+
+        }
+    }
+}
