f9cf64568c1f74d1ab96597890ddd1ace15abb7f
Fehiman <95622259+Fehiman@users.noreply.github.com>
2022-04-01 00:10:37 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace console_uygulama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            byte b=5;
+            sbyte c=5;
+            short s=5;
+            ushort us=5;
+
+            Int16 i16=2;
+            int i=2;
+            Int32 i32=2;
+            Int64 i64=2;
+
+            uint ui=2;
+            long l=4;
+            ulong ul=4;
+
+            float f=5;
+            double d=5;
+            decimal de=5;
+
+            char ch='2';
+            string str="Fehiman";
+
+            bool b1=true;
+            bool b2=false;
+
+            DateTime dt = DateTime.Now;
+            console.WriteLine(dt)
+        }
+    }
+}
